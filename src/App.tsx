c945d2ef452eda97dd64bcfdc44b52f49f719754
Tiagotj7 <tiagowork07@gmail.com>
2025-04-01 10:14:55 -0300

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import gitlogo from './git dev.png';
+import linklogo from './link dev.png'
+import instalogo from './insta logo dev.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <nav>
+      <ul>
+        <a href="https://github.com/Tiagotj7/">
+          <img src={gitlogo} className='gitlogo-app' alt="GitHub" />
         </a>
+        <a href="https://www.linkedin.com/in/tiagocarvalhog2020/">
+          <img src={linklogo} className='linklogo-app' alt="Linkedin" />
+          </a>
+        <a href="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/tiagotj7.dev/">
+          <img src={instalogo} className='instalogo-app' alt="Instagram" />
+        </a>
+      </ul>
+    </nav>
       </header>
+
+<footer></footer>
+
     </div>
   );
 }
